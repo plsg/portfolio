4fd1061a70c869884a88fd2b87c49cc6f5ec801b
plsg <patrick@patricklesage.ca>
2014-01-29 23:49:09 -0500

    fist commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Hi, this is for the presentation of a couple not-for-profit web sites that I am working on.
+
+I'm using my domain hosting to give everyone access to the current work.
+
+patricklesage.ca
---
+++ vo/presentation/js/menu.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function(){
+
+    //When mouse rolls over
+    $("#menu").mouseover(function(){
+        $(this).stop().animate({opacity:'.9',height:'50px'},{queue:false, duration:600, easing: 'jswing'})
+    });
+
+    //When mouse is removed
+    $("#menu").mouseout(function(){
+        $(this).stop().animate({opacity:'0',height:'40px'},{queue:false, duration:600, easing: 'jswing'})
+    });
+
+});
